4636cdf8b4d3ca8bdb59a1fcecae27df7d296683
SOMMY\Somesh <mahendrasomesh@gmail.com>
2018-05-13 10:32:33 +0530

    Added Drag Functionality on Object

--- Script/Manager/InputManager/Input.cs
+++ Script/Manager/InputManager/Input.cs
@@ -6,6 +6,8 @@ namespace SuperCatch
 {
     public class Input
     {
+        private static bool dragFlag = false;
+
         public static bool OnTap(GameObject gameObject)
         {
             if(UnityEngine.Input.GetMouseButtonDown(0))
@@ -21,6 +23,34 @@ namespace SuperCatch
             }
 
             return false;
+        }
+
+        public static bool OnTapRelease(GameObject gameObject)
+        {
+            if (UnityEngine.Input.GetMouseButtonUp(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool OnDrag(GameObject gameObject)
+        {
+            if(Input.OnTap(gameObject))
+                dragFlag = true;
+
+            else if(Input.OnTapRelease(gameObject) || UnityEngine.Input.GetMouseButtonUp(0))
+                dragFlag = false;
+
+            return dragFlag;
         }
     }
 }
