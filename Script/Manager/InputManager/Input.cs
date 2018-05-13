80c017c22c0c62649ce44b4cf354cdb40b9177c8
SOMMY\Somesh <mahendrasomesh@gmail.com>
2018-05-13 09:11:43 +0530

    InputManager GameObject Added

---
+++ Script/Manager/InputManager/Input.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    public class Input
+    {
+        public static bool OnTap(GameObject gameObject)
+        {
+            if(UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+
+                if(Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ Script/Manager/InputManager/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    public class InputManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
+
