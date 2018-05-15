99e5ba6b94e3db8e02fda34634fe89a0bb39fb2c
SOMMY\Somesh <mahendrasomesh@gmail.com>
2018-05-15 11:01:13 +0530

    Physics an BallManager Updated

---
+++ Script/Manager/BallManager/BallManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    public class BallManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject ball;
+        [SerializeField]
+        private GameObject ballStrikePoint;
+        
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            Vector3 ballVelocity = Physics.ProjectileVelocity(ball.transform.position, ballStrikePoint.transform.position, 30);
+            Debug.DrawRay(ball.transform.position, ballVelocity, Color.red);
+        }
+    }
+}
---
+++ Script/Physics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    public class Physics
+    {
+        public static Vector3 ProjectileVelocity(Vector3 sourcePosition, Vector3 targetPosition, float angleOfProjection)
+        {
+            Vector3 horizontalRangeDirection = (targetPosition - sourcePosition);
+            float horizontalRangeMagnitude = horizontalRangeDirection.magnitude;
+
+
+
+            return horizontalRangeDirection;
+        }
+    }
+}
+
