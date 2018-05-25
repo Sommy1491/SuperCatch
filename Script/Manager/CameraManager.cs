d9f81d80578c59c03fb619ed21ab23988194981b
SOMMY\Somesh <mahendrasomesh@gmail.com>
2018-05-25 10:55:00 +0530

    Added Camera Manager to define mouse position irrepective of camera orientation

---
+++ Script/Manager/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    public class CameraManager
+    {
+        public static Vector3 ScreenToWorldPoint(Vector3 pointOnPlane, Vector3 screenPoint)
+        {
+            Vector3 projectionVector = Vector3.Project(pointOnPlane, UnityEngine.Camera.main.transform.forward);
+            screenPoint.z = projectionVector.magnitude;
+
+            return UnityEngine.Camera.main.ScreenToWorldPoint(screenPoint);
+        }
+    }
+}
+
+
+
