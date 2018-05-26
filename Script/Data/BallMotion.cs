0c8aef61ad98e0b6f07b18131e2251476e5c4fd3
SOMMY\Somesh <mahendrasomesh@gmail.com>
2018-05-26 08:45:36 +0530

    Scriptable Object Added for storing path data

---
+++ Script/Data/BallMotion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    [CreateAssetMenu(fileName = "NewScriptableObject", menuName = "ScriptableObject")]
+    public class BallMotion : ScriptableObject
+    {
+        public Path[] paths; 
+    }
+}
---
+++ Script/Data/Path.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperCatch
+{
+    [System.Serializable]
+    public class Path
+    {
+        public Vector3 point_1;//source point
+        public Vector3 point_2;//destination point
+        public float angle;//Angle of Projection
+        public float pathSpeed;
+        public bool isTruePhysics = false;
+    }
+}
+
